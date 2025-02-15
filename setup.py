906a0b93fab5c232e6613f3d40548b72e5f5cd5b
hallko1234 <149827893+hallko1234@users.noreply.github.com>
2025-02-15 16:11:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 __version__ = '0.1.1'
 
-
 setup(
     name='seafileapi',
     version=__version__,
@@ -14,5 +13,18 @@ setup(
     platforms='any',
     packages=find_packages(),
     install_requires=['requests'],
-    classifiers=['Programming Language :: Python'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        # und so weiter, je nachdem was Ihr unterstützt
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+    # Falls ihr Python <3 nicht mehr unterstützen wollt,
+    # könnt ihr das hier explizit festlegen:
+    python_requires='>=3.6',
 )
+
